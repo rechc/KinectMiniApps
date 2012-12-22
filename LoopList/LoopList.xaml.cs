dcdc63082886608fb6a1e67f417c1aebcc41e76e
stas <oot.pac@googlemail.com>
2012-12-22 19:13:17 +0100

    margins

--- LoopList/LoopList.xaml.cs
+++ LoopList/LoopList.xaml.cs
@@ -23,52 +23,74 @@ namespace LoopList
     public partial class LoopList : UserControl
     {
         private List<String> pathList = new List<String>();
-        private Image left, center, right;
+        private Border left, center, right;
         private int centerIndex;
         private int dragging;
 
         public LoopList()
         {
             InitializeComponent();
-            
-            left = new Image();
-            left.Stretch = Stretch.Fill;
+
+            left = new Border();
+            left.BorderThickness = new Thickness(10);
             left.RenderTransform = new TranslateTransform();
+            left.BorderBrush = new SolidColorBrush(Color.FromArgb(255, 255, 255, 255));
 
-            center = new Image();
-            center.Stretch = Stretch.Fill;
+            center = new Border();
+            center.BorderThickness = new Thickness(10);
             center.RenderTransform = new TranslateTransform();
-
-            right = new Image();
-            right.Stretch = Stretch.Fill;
+            center.BorderBrush = new SolidColorBrush(Color.FromArgb(255, 255, 255, 255));
+            
+            right = new Border();
+            right.BorderThickness = new Thickness(10);
             right.RenderTransform = new TranslateTransform();
+            right.BorderBrush = new SolidColorBrush(Color.FromArgb(255, 255, 255, 255));
 
-            rootGrid.Children.Add(left);
-            rootGrid.Children.Add(center);
-            rootGrid.Children.Add(right);
+            Image leftImg = new Image();
+            leftImg.Stretch = Stretch.Fill;
+
+            Image centerImg = new Image();
+            centerImg.Stretch = Stretch.Fill;
+
+            Image rightImg = new Image();
+            rightImg.Stretch = Stretch.Fill;
+
+            left.Child = leftImg;
+            center.Child = centerImg;
+            right.Child = rightImg;
+
 
             Loaded += delegate
             {
                 TranslateTransform ttLeft = (TranslateTransform)left.RenderTransform;
-                ttLeft.X = -left.ActualWidth;
+                ttLeft.X = -left.ActualWidth + center.ActualWidth * 0.1;
+
                 TranslateTransform ttRight = (TranslateTransform)right.RenderTransform;
-                ttRight.X = right.ActualWidth;
+                ttRight.X = right.ActualWidth - center.ActualWidth * 0.1;
+
+                rootGrid.Margin = new Thickness(center.ActualWidth * 0.05, 0, center.ActualWidth * 0.05, 0);
             };
+
+
+            rootGrid.Children.Add(left);
+            rootGrid.Children.Add(center);
+            rootGrid.Children.Add(right);
         }
 
         public void add(string path)
         {
             if (pathList.Count == 0)
             {
-                center.Source = loadData(path);
-                left.Source = center.Source;
-                right.Source = center.Source;
+                ((Image)center.Child).Source = loadData(path);
+                ((Image)left.Child).Source = ((Image)center.Child).Source;
+                ((Image)right.Child).Source = ((Image)center.Child).Source;
+                
             }
-            left.Source = loadData(path);
+            ((Image)left.Child).Source = loadData(path);
             if (pathList.Count == 1)
             {
-                
-                right.Source = left.Source;
+
+                ((Image)right.Child).Source = ((Image)left.Child).Source;
             }
             
             pathList.Add(path);
@@ -108,6 +130,10 @@ namespace LoopList
             if (dragging == 0)
             {
                 dragging = 2;
+                Grid.SetZIndex(left, 0);
+                Grid.SetZIndex(center, 1);
+                Grid.SetZIndex(right, 1);
+
                 TranslateTransform ttCenter = (TranslateTransform)center.RenderTransform;
                 TranslateTransform ttRight = (TranslateTransform)right.RenderTransform;
                 TranslateTransform ttLeft = (TranslateTransform)left.RenderTransform;
@@ -115,26 +141,26 @@ namespace LoopList
                 DoubleAnimation doubleAnimationCenter = new DoubleAnimation();
                 doubleAnimationCenter.From = ttCenter.X;
                 doubleAnimationCenter.To = -center.ActualWidth;
-                doubleAnimationCenter.Duration = new Duration(new TimeSpan(0, 0, 0, 0, 200));
+                doubleAnimationCenter.Duration = new Duration(new TimeSpan(0, 0, 0, 0, 150));
                 doubleAnimationCenter.Completed += (s, _) => FadeAnimationCompleted(ttCenter, doubleAnimationCenter.To);
                 ttCenter.BeginAnimation(TranslateTransform.XProperty, doubleAnimationCenter);
 
                 DoubleAnimation doubleAnimationRight = new DoubleAnimation();
                 doubleAnimationRight.From = ttRight.X;
                 doubleAnimationRight.To = 0;
-                doubleAnimationRight.Duration = new Duration(new TimeSpan(0, 0, 0, 0, 200));
+                doubleAnimationRight.Duration = new Duration(new TimeSpan(0, 0, 0, 0, 150));
                 doubleAnimationRight.Completed += (s, _) => FadeAnimationCompleted(ttRight, doubleAnimationRight.To);
                 ttRight.BeginAnimation(TranslateTransform.XProperty, doubleAnimationRight);
 
                 ttLeft.X = left.ActualWidth;
 
-                Image tmp = left;
+                Border tmp = left;
                 left = center;
                 center = right;
                 right = tmp;
                 centerIndex = nextIndex(centerIndex);
                 int indexForRight = nextIndex(centerIndex);
-                right.Source = loadData(pathList[indexForRight]);
+                ((Image)right.Child).Source = loadData(pathList[indexForRight]);
             }
         }
 
@@ -150,6 +176,9 @@ namespace LoopList
             if (dragging == 0)
             {
                 dragging = 2;
+                Grid.SetZIndex(right, 0);
+                Grid.SetZIndex(center, 1);
+                Grid.SetZIndex(left, 1);
                 TranslateTransform ttCenter = (TranslateTransform)center.RenderTransform;
                 TranslateTransform ttRight = (TranslateTransform)right.RenderTransform;
                 TranslateTransform ttLeft = (TranslateTransform)left.RenderTransform;
@@ -157,28 +186,26 @@ namespace LoopList
                 DoubleAnimation doubleAnimationCenter = new DoubleAnimation();
                 doubleAnimationCenter.From = ttCenter.X;
                 doubleAnimationCenter.To = center.ActualWidth;
-                doubleAnimationCenter.Duration = new Duration(new TimeSpan(0, 0, 0, 0, 200));
+                doubleAnimationCenter.Duration = new Duration(new TimeSpan(0, 0, 0, 0, 150));
                 doubleAnimationCenter.Completed += (s, _) => FadeAnimationCompleted(ttCenter, doubleAnimationCenter.To);
                 ttCenter.BeginAnimation(TranslateTransform.XProperty, doubleAnimationCenter);
 
                 DoubleAnimation doubleAnimationLeft = new DoubleAnimation();
                 doubleAnimationLeft.From = ttLeft.X;
                 doubleAnimationLeft.To = 0;
-                doubleAnimationLeft.Duration = new Duration(new TimeSpan(0, 0, 0, 0, 200));
+                doubleAnimationLeft.Duration = new Duration(new TimeSpan(0, 0, 0, 0, 150));
                 doubleAnimationLeft.Completed += (s, _) => FadeAnimationCompleted(ttLeft, doubleAnimationLeft.To);
                 ttLeft.BeginAnimation(TranslateTransform.XProperty, doubleAnimationLeft);
 
                 ttRight.X = -right.ActualWidth;
 
-
-
-                Image tmp = right;
+                Border tmp = right;
                 right = center;
                 center = left;
                 left = tmp;
                 centerIndex = previousIndex(centerIndex);
                 int indexForLeft = previousIndex(centerIndex);
-                left.Source = loadData(pathList[indexForLeft]);
+                ((Image)left.Child).Source = loadData(pathList[indexForLeft]);
             }
         }
 
