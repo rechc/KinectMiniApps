6cc60e7d943412e0e2bd3a72ebb9584294172f1b
RechC <rech.christian@gmail.com>
2013-01-31 12:08:29 +0100

    Use EntityFramework to interact with database - created local database - add entity db schema - add poco (Plain Old CLR/C# Object) template genarator - DAO (DataAccessObject)

---
+++ Database/DAO/TravelOverDAO.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.DAO
+{
+    public class TravelOverDAO
+    {
+        private readonly Model1Container _context;
+        private TravelOffer _offer;
+
+        public TravelOverDAO(Model1Container context)
+        {
+            _context = context;
+        }
+
+        private bool IsNew()
+        {
+            return _offer.OfferId == 0;
+        }
+
+        private void Insert()
+        {
+            _context.TravelOfferSet.Add(_offer);
+        }
+
+        private void Update()
+        {
+            var offerEntity = _context.TravelOfferSet.Single(o => o.OfferId == _offer.OfferId);
+            _context.Entry(offerEntity).CurrentValues.SetValues(_offer);
+        }
+
+        public void Save(TravelOffer offer)
+        {
+            _offer = offer;
+            if (IsNew())
+                Insert();
+            else
+                Update();
+        }
+    }
+}
