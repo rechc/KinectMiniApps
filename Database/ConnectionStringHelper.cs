26ab3a158c6ac96e6087aa67124fd9b333337939
RechC <rech.christian@gmail.com>
2013-02-04 22:20:31 +0100

    add a connection string helper class

---
+++ Database/ConnectionStringHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.EntityClient;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    public static class ConnectionStringHelper
+    {
+        public static string GetSqlCeConnectionString(string fileName)
+        {
+            var csBuilder = new EntityConnectionStringBuilder();
+
+            csBuilder.Provider = "System.Data.SqlServerCe.3.5";
+            csBuilder.ProviderConnectionString = string.Format("Data Source={0};", fileName);
+
+            csBuilder.Metadata = string.Format("res://{0}/Model1.csdl|res://{0}/Model1.ssdl|res://{0}/Model1.msl",
+                typeof(Model1Container).Assembly.FullName);
+
+            return csBuilder.ToString();
+        }
+
+        public static string GetSqlConnectionString(string serverName, string databaseName)
+        {
+            SqlConnectionStringBuilder providerCs = new SqlConnectionStringBuilder();
+
+            providerCs.DataSource = serverName;
+            providerCs.InitialCatalog = databaseName;
+            providerCs.IntegratedSecurity = true;
+
+            var csBuilder = new EntityConnectionStringBuilder();
+
+            csBuilder.Provider = "System.Data.SqlClient";
+            csBuilder.ProviderConnectionString = providerCs.ToString();
+
+            csBuilder.Metadata = string.Format("res://{0}/Model1.csdl|res://{0}/Model1.ssdl|res://{0}/Model1.msl",
+                typeof(Model1Container).Assembly.FullName);
+
+            return csBuilder.ToString();
+        }
+    }
+}
