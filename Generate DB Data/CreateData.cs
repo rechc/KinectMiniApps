059b3bee2ad0ca0eb28ae7badbcc3afa3fdd2d29
RechC <rech.christian@gmail.com>
2013-02-14 14:17:21 +0100

    added helper methods image to byte array and byte arry to image

--- Generate DB Data/CreateData.cs
+++ Generate DB Data/CreateData.cs
@@ -10,47 +10,8 @@ using Database;
 
 namespace Generate_DB_Data
 {
-    class CreateData
+    partial class CreateData
     {
-        private static string ConnectionString
-        {
-            get
-            {
-                // Specify the provider name, server and database.
-                string providerName = "System.Data.SqlClient";
-                string serverName = "localhost";
-                string databaseName = "AdventureWorks";
-
-                // Initialize the connection string builder for the
-                // underlying provider.
-                SqlConnectionStringBuilder sqlBuilder =
-                    new SqlConnectionStringBuilder();
-
-                // Set the properties for the data source.
-                sqlBuilder.DataSource = serverName;
-                sqlBuilder.InitialCatalog = databaseName;
-                sqlBuilder.IntegratedSecurity = true;
-
-                // Build the SqlConnection connection string.
-                string providerString = sqlBuilder.ToString();
-
-                // Initialize the EntityConnectionStringBuilder.
-                EntityConnectionStringBuilder entityBuilder =
-                    new EntityConnectionStringBuilder();
-
-                //Set the provider name.
-                entityBuilder.Provider = providerName;
-
-                // Set the provider-specific connection string.
-                entityBuilder.ProviderConnectionString = providerString;
-
-                // Set the Metadata location.
-                entityBuilder.Metadata = "res://*/";
-
-                return entityBuilder.ToString();
-            }
-        }
-
         private Model1Container _context;
         private readonly Random _random = new Random();
 
---
+++ Generate DB Data/Helper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generate_DB_Data
+{
+    partial class CreateData
+    {
+        public Image ByteArrayToImage(byte[] fileBytes)
+        {
+            using (MemoryStream memStream = new MemoryStream(fileBytes))
+            {
+                return Image.FromStream(memStream);
+            }
+        }
+
+        public byte[] ImageToByteArray(Image imageIn)
+        {
+            using (MemoryStream memStream = new MemoryStream())
+            {
+                imageIn.Save(memStream, System.Drawing.Imaging.ImageFormat.Gif);
+                return memStream.ToArray();
+            }
+        }
+
+    }
+}
