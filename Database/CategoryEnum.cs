3eff6990095061da071f811feea46e920188d900
RechC <rech.christian@gmail.com>
2013-02-14 11:09:13 +0100

    refactoring database shema

---
+++ Database/CategoryEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    public enum CategoryEnum
+    {
+        Top,
+        Beach,
+        Ski,
+        City,
+        Wander
+    }
+}
