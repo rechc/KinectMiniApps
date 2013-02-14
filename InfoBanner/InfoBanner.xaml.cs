0b45ec0702ef003b1a5a52948aea1a9441838f16
RechC <rech.christian@gmail.com>
2013-02-14 11:08:40 +0100

    InfoBanner design layout

--- InfoBanner/InfoBanner.xaml.cs
+++ InfoBanner/InfoBanner.xaml.cs
@@ -21,12 +21,20 @@ namespace InfoBanner
             Rating.Text = "Bewertung: " + offer.HotelRating;
             HotelName.Text = offer.HotelName;
             Place.Text = offer.Place;
-            PricePerPerson.Text = offer.PricePerPerson + "€\n pro Person";
+            PricePerPerson.Text = offer.PricePerPerson + ",-\n pro Person";
             TravelInfo.Text = offer.DayCount + " tägige " + offer.TravelType + "\ninkl. " + offer.BoardType;
+
             string extInfo = "";
             foreach (ExtendedInformation information in offer.ExtendedInformation)
                 extInfo += ("-" + information.Information + "\n");
             ExtendetInfo.Text = extInfo;
+
+            //var iterator = offer.ExtendedInformation.GetEnumerator();
+            ////ExtendetInfoImg1.Source = new BitmapImage(){ Source = new BitmapImage(new Uri(Directory.GetFiles(Environment.CurrentDirectory + @"\Images\Arrow.png").First(), UriKind.RelativeOrAbsolute)), Stretch = Stretch.Fill };
+            //ExtendetInfo1.Text = (iterator.MoveNext()) ? iterator.Current.Information : "";
+            //ExtendetInfo2.Text = (iterator.MoveNext()) ? iterator.Current.Information : "";
+            //ExtendetInfo3.Text = (iterator.MoveNext()) ? iterator.Current.Information : "";
+            //ExtendetInfo4.Text = (iterator.MoveNext()) ? iterator.Current.Information : "";
         }
     }
 }
