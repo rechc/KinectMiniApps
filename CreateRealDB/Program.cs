5c0bc12b5bed39014e5a3ad8369dc8ee12697b4c
Andreas W <mapsmail@gmx.de>
2013-02-14 12:48:51 +0100

    beispiel daten 0.1

--- CreateRealDB/Program.cs
+++ CreateRealDB/Program.cs
@@ -10,6 +10,31 @@ namespace CreateRealDB
 {
     class DBFill
     {
+        public Model1Container _context;
+
+        private void CreateCountryEntries()
+        {
+                //string[] countries = {"Spanien", "Deutschland", "USA", "Mallorca", "Frankreich"};
+                foreach (var countryName in Enum.GetNames(typeof(CategoryEnum)))
+                {
+                    var c = new Category() { CategoryName = countryName };
+                    _context.CategorySet.Add(c);
+                } 
+        }
+
+        private void SaveToDb()
+        {
+                try
+                {
+                   _context.SaveChanges(); 
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    Console.WriteLine("Error in entry. Start db rollback");
+                }
+        }
+
         public void CreateOffers(int price, String place, int rating, String hotelname, String anfahrt, int daycount, String futtertyp, CategoryEnum category, String image, bool top)
         {
             var dao = new TravelOfferDao();
@@ -37,22 +62,24 @@ namespace CreateRealDB
 
         public void FlushDbData()
         {
-            using (var context = new Model1Container())
-            {
-                if (context.Database.Exists())
+                if (_context.Database.Exists())
                 {
-                  context.Database.Delete();
+                  _context.Database.Delete();
                 }
-                context.Database.CreateIfNotExists();
-            }
+                _context.Database.CreateIfNotExists();
         }
 
         static void Main(string[] args)
         {
             DBFill dbf = new DBFill();
-            dbf.FlushDbData();
+            using (var con = new Model1Container())
+            {
+                dbf._context = con;
+                dbf.FlushDbData();
+                dbf.CreateCountryEntries();
+                dbf.SaveToDb(); 
+            }
             dbf.CreateMyOffers();
-            
         }
     }
 }
